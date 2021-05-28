d8bd6dcf6c2515bf44478590537e01d11120a16b
Sayan Chatterjee <sayanchatterjee2000@gmail.com>
2021-05-28 08:34:06 +0530

    added option to withdraw

--- routers/candidate.js
+++ routers/candidate.js
@@ -3,9 +3,11 @@ const express = require('express');
 const useQuery = require('../useQuery');
 const verify = require('../verify');
 
+const table = 'candidates';
+const table2 = 'election_records';
+
 var createRouter = (db) => {
     var router = express.Router();
-    let table = 'candidates';
     router.get('/candidate', verify(), (req, res) => {
         let sql = `SELECT eid FROM ${table} WHERE username = ?`;
         useQuery(db, sql, [req.user.username])
@@ -14,6 +16,28 @@ var createRouter = (db) => {
             res.send(result);
         })
     })
+    router.get('/candidate/withdraw/:eid', verify(), (req,res) => {
+        let sql = `SELECT elec_status FROM ${table2} WHERE eid = ?`;
+        let sql2 = `DELETE FROM ${table} WHERE username = ? AND eid = ?`; //check
+        useQuery(db,sql,[req.params.eid])
+        .then((result)=>{
+            if(result.length === 0) return res.status(400).send("No election with this Election ID")
+            if(result.length > 1) return res.status(500).send("Error")
+            if(result[0].elec_status === 1) return res.status(400).send("Cannot Withdraw Now as Election Has been Started")
+            if(result[0].elec_status === 2) return res.status(400).send("Election Has Ended")
+            else if(result[0].elec_status === 0){
+                useQuery(db,sql2,[req.user.username, req.params.eid])
+                .then((result2)=>{
+                    if(result2.length === 0) return res.status(400).send("You are not a Candidate in this election")
+                    res.status(200).send({
+                        withdrew : result,
+                        message : "Withdraw Successful"
+                    })
+                })
+            }            
+        })
+    })
+    
     return router;
 }
 
